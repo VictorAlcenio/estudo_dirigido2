90d2f8cba11bdc23d2f60ccade4296b490ac1a46
VictorAlcenio <48133710+VictorAlcenio@users.noreply.github.com>
2019-04-25 20:49:56 -0300

    algoritmo em desenvolvimento

---
+++ exercicio11.php
@@ -0,0 +1,7 @@
+<?php
+
+#Faça um Programa que leia um número inteiro menor que 1000 e imprima a quantidade de centenas,
+#dezenas e unidades do mesmo, observando os termos no plural a colocação do "e", da vírgula entre
+#outros. Exemplo:
+#326 = 3 centenas, 2 dezenas e 6 unidades
+#12 = 1 dezena e 2 unidades

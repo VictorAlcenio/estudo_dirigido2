5020e24dca246241ae9ff00f3d0fa2681b367768
VictorAlcenio <48133710+VictorAlcenio@users.noreply.github.com>
2019-04-29 21:15:59 -0300

    desenvolvimento do algoritmo

--- exercicio20.php
+++ exercicio20.php
@@ -1,3 +1,4 @@
+<?php
 
 #Faça um algoritmo que imprima as seguintes figuras geométricas:
 #* * * * * * * *
@@ -20,4 +21,47 @@
 
                                                        
                                                     # obs: falta uma 
+    
+    
+         for ($t=1; $t <= 8; $t++ ) { 
+       
+            #
+         for ($i=1; $i <= 8 ; $i++) { 
+                                                             
+        print "*  ";
+         }
+        print "\n";
+        }
+    
+    
+    
+    print "\n\n";
+    
+#---------------------------------------------------
+    
+    for ($t=1; $t <= 8; $t++) { 
+       
+     for ($i=1; $i <= $t ; $i++) { 
+                                                             
+        print "*  ";
+        }
+        print "\n";
+        }
+
+        
+        print "\n\n";
+    
+#-------------------------------------------------    
+        
+         for ($i=0; $i<=8; $i++){ 
+    
+         for($h=0; $h<=8-$i; $h++){
+             print "  ";
+         }
+         for($h=0; $h<$i; $h++){
+             print "* ";
+         }
+         
+         print "\n";
+     }
     
